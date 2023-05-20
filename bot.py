eedd71c27cd7e5aedbe71c703028c85f82518142
Chris Wood <woodc@us.ibm.com>
2023-05-20 14:21:21 -0400

    Added argument description for roll help. Added ability to mod the dice roll command.

--- bot.py
+++ bot.py
@@ -106,10 +106,24 @@ async def greeting(ctx):
 async def roll(
     ctx,
     dice_str: str = commands.parameter(
-        description="Valid dice roll string -- ie, 2d8 for 2 x 8-sided dice"
+        description="Valid dice roll string with +/- mod -- ie, 2d8+1 for 2 x 8-sided dice + 1"
     ),
 ):
-    sides_and_dice = dice_str.split("d")
+    # Pull any Mod out of the string
+    mod = 0
+    mod_op = "+"
+    dice = dice_str
+    if "+" in dice_str:
+        dice_and_mod = dice_str.split("+")
+        dice = dice_and_mod[0]
+        mod = dice_and_mod[-1]
+    elif "-" in dice_str:
+        dice_and_mod = dice_str.split("-")
+        dice = dice_and_mod[0]
+        mod_op = "-"
+        mod = dice_and_mod[-1]
+    # Split string into Num of Dice and Dice Sides
+    sides_and_dice = dice.split("d")
     if len(sides_and_dice) != 2 or any(not n.isnumeric() for n in sides_and_dice):
         bot.logger.error(f"{dice_str} is not a valid format for the roll command")
         await ctx.send(f"**{dice_str}** is not a valid format for the roll command")
@@ -118,13 +132,23 @@ async def roll(
     dice = int(sides_and_dice[0])
     roll = 0
     inv_rolls = []
+    # Sum up the sides while keeping track of the individual rolls
     for i in range(dice):
         inv_rolls.append(random.choice(range(1, sides + 1)))
         roll += inv_rolls[-1]
+    # Evaluate the mod
+    roll = eval(f"roll {mod_op} {mod}")
+    # Null out vars if mod is zero
+    if mod == 0:
+        mod_op = ""
+        mod = ""
+    # Log and return output as a message
     bot.logger.info(
-        f"Rolling a {dice}d{sides} for {ctx.author.name} --> {roll} {inv_rolls}"
+        f"Rolling a {dice}d{sides}{mod_op}{mod} for {ctx.author.name} ==> {roll} ({inv_rolls}{mod_op}{mod})"
     )
-    await ctx.send(f"{dice}d{sides} --> **{roll}** {inv_rolls}")
+    await ctx.send(
+        f"{dice}d{sides}{mod_op}{mod} ==> **{roll}** ({inv_rolls}{mod_op}{mod})"
+    )
 
 
 bot.run(settings.discord_token)
